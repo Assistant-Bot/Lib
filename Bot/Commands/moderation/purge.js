997ec333d69845d1dab5de097a715e0b8f018549
Olybear9 <olybear9@gmail.com>
2019-06-23 16:16:37 -0600

    Update purge.js

--- Bot/Commands/moderation/purge.js
+++ Bot/Commands/moderation/purge.js
@@ -20,8 +20,12 @@ class Purge {
             if (amount > 2000) return Util.sendError(msg, emojis, 'custom', 'You can not purge more than 2000 messages at a time.');
             if (!msg.guild.members.get(bot.user.id).permission.has('manageMessages')) return Util.sendError(msg, emojis, 'perm', 'Manage Message');
             try {
-                await msg.channel.bulkDelete(amount);
-                msg.channel.send(emojis.greentick + ' Purged `' + amount + '` messages.');
+                m = await msg.channel.send(emojis.loading + ' Purging `' + amount + '` messages..');
+                await msg.channel.purge(amount, (ms) => {
+                    if (ms.id == m.id) return false; 
+                    else return true;
+                });
+                m.edit(emojis.greentick + ' Purged `' + amount + '` messages.');
             } catch (e) {
                 Util.logError(bot, msg, args, Util, emojis, this, e);
                 return Util.sendError(msg, emojis, 'unknown');
