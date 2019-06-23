df72003349110a5ef37742961d019311b2fe57e3
Olybear9 <olybear9@gmail.com>
2019-06-23 17:25:14 -0600

    Update banlist.js

--- Bot/Commands/moderation/banlist.js
+++ Bot/Commands/moderation/banlist.js
@@ -11,25 +11,29 @@ class BanList {
     async onRun(bot, msg, args, Util, emojis) {
         if (!msg.guild.members.get(bot.user.id).permission.has('banMembers')) return Util.sendError(msg, emojis, 'perm', 'Ban Members');
         if (!msg.guild.members.get(bot.user.id).permission.has('embedLinks')) return Util.sendError(msg, emojis, 'perm', 'Embed Links');
+        try {
+            let m = await msg.channel.send(emojis.loading + ' Gathering bans...');
+            let bans = await msg.guild.getBans();
+            let pretty = [];
+            let i = 1;
+            let cache = bans.length;
+            bans = bans.slice(0, 50);
 
-        let m = await msg.channel.send(emojis.loading + ' Gathering bans...');
-        let bans = await msg.guild.getBans();
-        let pretty = [];
-        let i = 1;
-        let cache = bans.length;
-        bans = bans.slice(0, 50);
+            await bans.forEach((ban) => {
+                pretty.push(`**${i}.)** ` + '__' + ban.user.username + '__ (`' + ban.user.id + ')`');
+            });
 
-        await bans.forEach((ban) => {
-            pretty.push(`**${i}.)** ` + '__' + ban.user.username + '__ (`' + ban.user.id + ')`');
-        }); 
+            let em = new Util.SimpleEmbed();
+            em.setColor('#fc5928');
+            em.setAuthor(msg.guild.name, msg.guild.iconURL);
+            em.setTitle('Showing 50 of ' + cache + 'bans');
+            em.setDescription(pretty.join('\n'));
 
-        let em = new Util.SimpleEmbed();
-        em.setColor('#fc5928');
-        em.setAuthor(msg.guild.name, msg.guild.iconURL);
-        em.setTitle('Showing 50 of ' + cache + 'bans');
-        em.setDescription(pretty.join('\n'));
-
-        msg.channel.send(em);
+            msg.channel.send(em);
+        } catch (e) {
+            Util.logError(bot, msg, args, Util, emojis, this, e);
+            return Util.sendError(msg, emojis, 'unknown');
+        }
     }
 
     async onNoPerm(bot, msg, args, Util, emojis) {
