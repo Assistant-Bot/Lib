f0d7b4351336d428f86b880e106404ef72a66874
Olybear9 <olybear9@gmail.com>
2019-06-23 04:06:25 -0600

    fix syntax ffs

--- Bot/Commands/owner/update.js
+++ Bot/Commands/owner/update.js
@@ -12,12 +12,11 @@ class Update {
         let branch = 'v1';
         try {
             if (args[0]) branch = args[0]
-                let cmd = args.slice(0).join(' ');
                 m = await msg.channel.send(`${emojis.loading} Attempting to update...`);
-                output = await Util.runCommand(cmd);
+                output = await Util.runCommand('git pull ' + branch);
                 console.log(output);
-                if (typeof output == 'object') return m.edit(emojis.redtick + ' Failed to pull from `' + branch + '`');
-                else return m.edit(emojis.greentick + ' Successfully pulled from `' + branch + '` - Restarting');
+            if (typeof output == 'object') return m.edit(emojis.redtick + ' Failed to pull from `' + branch + '`');
+             else return m.edit(emojis.greentick + ' Successfully pulled from `' + branch + '` - Restarting');
         } catch (e) {
             return m.edit(`${emojis.redtick} Failed to run command: ${e}`);
         }
