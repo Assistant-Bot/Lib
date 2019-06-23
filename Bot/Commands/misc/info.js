1ddd13486045877591883ef566521e5b2ceed416
Olybear9 <olybear9@gmail.com>
2019-06-23 15:51:11 -0600

    Update info.js

--- Bot/Commands/misc/info.js
+++ Bot/Commands/misc/info.js
@@ -19,8 +19,8 @@ class info {
         em.setColor('#26d1ff');
         em.addField('Version', process.version, true);
         em.addField('Library', 'Eris\nAssistant v2', true);
-        em.addField('Users', bot.users.size, true);
         em.addField('Support', '[Support Server](https://discord.gg/FKTrmsK)', true);
+        em.addField('Users', bot.users.size, true);
         em.addField('Servers', bot.guilds.size, true);
         em.addField('Invite', '[Invite]()', true);
         em.setFooter('Assistant V2', bot.user.avatarURL);
