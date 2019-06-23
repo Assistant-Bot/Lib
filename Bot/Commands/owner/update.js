4cbc5b1932d5f841a7e215e074c9f51296aac803
Olybear9 <olybear9@gmail.com>
2019-06-23 04:10:07 -0600

    Update update.js

--- Bot/Commands/owner/update.js
+++ Bot/Commands/owner/update.js
@@ -10,13 +10,20 @@ class Update {
     async onRun(bot, msg, args, Util, emojis) {
         let m, output;
         let branch = 'v1';
+        let restart = false;
         try {
-            if (args[0]) branch = args[0]
+            if (args[0]) branch = args[0];
                 m = await msg.channel.send(`${emojis.loading} Attempting to update...`);
                 output = await Util.runCommand('git pull origin ' + branch);
                 console.log(output);
             if (typeof output == 'object') return m.edit(emojis.redtick + ' Failed to pull from `' + branch + '`');
-             else return m.edit(emojis.greentick + ' Successfully pulled from `' + branch + '` - Restarting');
+             else {
+                 if (!restart) return m.edit(emojis.greentick + ' Successfully updated! Pulled from: `' + branch + '`.');
+                 else {
+                     m.edit(emojis.greentick + ' Successfully updated! Pulled from: `' + branch + '`. Restarting!');
+                     process.exit();
+                 }
+             }
         } catch (e) {
             return m.edit(`${emojis.redtick} Failed to run command: ${e}`);
         }
