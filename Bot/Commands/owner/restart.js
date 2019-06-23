07f42995d959fdc704b9dd9ac45d7e18e5565638
Olybear9 <olybear9@gmail.com>
2019-06-23 04:19:26 -0600

    Update Commands
    - Update the update command, cutie...
    - Add restart :heart:

---
+++ Bot/Commands/owner/restart.js
@@ -0,0 +1,36 @@
+class Restart {
+    constructor() {
+        this.name = 'restart';
+        this.aliases = [];
+        this.description = '/reload [command]';
+        this.longDescription = 'Reload commands on the bot.';
+        this.list = false;
+    }
+
+    async onRun(bot, msg, args, Util, emojis) {
+        let m;
+
+        try {
+            m = await msg.channel.send(`${emojis.check} Restarting...`);
+            process.exit();
+        } catch (e) {
+            return m.edit(`${emojis.redtick} Restart failed: ${e}`);
+        }
+    }
+
+    async onError(bot, msg, args, Util, emojis) {
+        Util.logError(bot, msg, args, Util, emojis, this);
+        return Util.sendError(msg, emojis, 'unknown');
+    }
+
+    async onNoPerm() {
+        return; // ghost the message, oooo spooky
+    }
+
+    async onPermCheck(bot, msg, args, Util, emojis) {
+        if (msg.author.id != '217006264570347520') return false;
+        else return true;
+    }
+}
+
+module.exports = Restart;
--- Bot/Commands/owner/update.js
+++ Bot/Commands/owner/update.js
@@ -10,21 +10,16 @@ class Update {
     async onRun(bot, msg, args, Util, emojis) {
         let m, output;
         let branch = 'v1';
-        let restart = true;
+
         try {
             if (args[0]) branch = args[0];
-            if (branch == 'yes') { branch = 'v1'; restart = false };
                 m = await msg.channel.send(`${emojis.loading} Attempting to update...`);
                 output = await Util.runCommand('git pull origin ' + branch);
             if (typeof output == 'object') return await m.edit(emojis.redtick + ' Failed to pull from `' + branch + '`');
              else {
-                 if (restart == false) return await m.edit(emojis.greentick + ' Successfully updated! Pulled from: `' + branch + '`.');
-                 else {
-                     await m.edit(emojis.greentick + ' Successfully updated! Pulled from: `' + branch + '`. Restarting!');
-                     await msg.channel.send('```xl\n' + output + '\n```');
-                     process.exit();
-                 }
-             }
+                    await m.edit(emojis.greentick + ' Successfully updated! Pulled from: `' + branch + '`.');
+                    await msg.channel.send('```xl\n' + output + '\n```');
+            }
         } catch (e) {
             return m.edit(`${emojis.redtick} Failed to run command: ${e}`);
         }
