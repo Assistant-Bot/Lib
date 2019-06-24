85600a4c70b40053461c2d106efa00871b3ad456
Olybear9 <olybear9@gmail.com>
2019-06-23 20:02:15 -0600

    Update blacklist.js

--- Bot/Commands/owner/blacklist.js
+++ Bot/Commands/owner/blacklist.js
@@ -27,20 +27,17 @@ class Blacklist {
                 em.setColor('#ff0000');
                 em.setTitle('All blacklisted users');
                 em.setDescription(clean.join('\n'));
-            }
-
-            if (args[0] == 'add') {
+            } else if (args[0] == 'add') {
                 if (!wrapped[1]) return Util.sendError(msg, emojis, 'custom', 'Invalid user.');
                 let reason = (!wrapped[2]) ? "No reason provided" : wrapped[2];
                 Util.blacklist(wrapped[1], 0, msg.author.id, reason);
                 return msg.channel.send(emojis.greentick + ' Successfully blacklisted `' + wrapped[0] + '` with reason: `' + reason + '`');
-            }
-            if (args[0] == 'remove') {
+            } else if (args[0] == 'remove') {
                 if (!wrapped[1]) return Util.sendError(msg, emojis, 'custom', 'Invalid user.');
                 let reason = (!wrapped[2]) ? "No reason provided" : wrapped[2];
                 Util.blacklist(wrapped[1], 1, msg.author.id, reason);
                 return msg.channel.send(emojis.greentick + ' Successfully unblacklisted `' + wrapped[0] + '` with reason: `' + reason + '`');
-            } else return Util.sendError(msg, emojis, 'custom', '**Usage:** a!blacklist <add/remove/view> [id] [reason]');
+            } else return Util.sendError(msg, emojis, 'custom', '**Usage:** a!blacklist <add/remove/view> [id] [reason] fuck: ' + args);
         } catch (e) {
             return msg.channel.send(`${emojis.redtick} Failed to run command: ${e}`);
         }
