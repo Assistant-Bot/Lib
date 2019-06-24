ac540aab57b79951e7920edc7abeca1afd4b9f87
Olybear9 <olybear9@gmail.com>
2019-06-23 20:34:02 -0600

    Update blacklist.js

--- Bot/Commands/owner/blacklist.js
+++ Bot/Commands/owner/blacklist.js
@@ -20,7 +20,7 @@ class Blacklist {
                 await keys.forEach((key) => {
                     i++;
                     let ob = blacklisted[key];
-                    clean.push(`**${i}.)** __${bot._restClient.getRESTUser(key).username} (\`${key}\`)__ - **M** (${bot._restClient.getRESTUser(ob.admin).username})`);
+                    clean.push(`**${i}.)** __${JSON.stringify(bot._restClient.getRESTUser(key).username)} (\`${key}\`)__ - **M** (${bot._restClient.getRESTUser(ob.admin).username})`);
                 });
 
                 if (keys.length == 0) return Util.sendError(msg, emojis, 'custom', 'No blacklisted users.');
