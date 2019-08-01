173e719992c5552da20ee28013083979d7b463a1
Olybear9 <olybear9@gmail.com>
2019-08-01 12:25:52 -0600

    Update uptime.js

--- Bot/Commands/misc/uptime.js
+++ Bot/Commands/misc/uptime.js
@@ -14,14 +14,14 @@ class Uptime {
         let days = Math.floor((temp %= 31536000) / 86400);
         let hours = Math.floor((temp %= 86400) / 3600);
         let minutes = Math.floor((temp %= 3600) / 60);
-        let seconds = temp % 60;
+        let seconds = Math.floor(temp % 60);
 
         days = (days < 1) ? '' : (days === 1) ? '**1** Day ' : `**${days}** Days `;
         hours = (hours < 1) ? '' : (hours === 1) ? '**1** hour ' : `**${hours}** hours `;
         minutes = (minutes < 1) ? '' : (minutes === 1) ? '**1** minute and ' : `**${minutes}** minutes and `;
         seconds = (seconds < 1) ? '' : (days === 1) ? '**1** second' : `**${seconds}** seconds`;
 
-        msg.channel.send(emojis.check + ` **Assistant v2** has been online for:\n> ${days}${hours}${minutes}${seconds}`);
+        msg.channel.send(emojis.check + ` **Assistant v2** has been online for: ${days}${hours}${minutes}${seconds}`);
     }
 
     async onError(bot, msg, args, Util, emojis) {
