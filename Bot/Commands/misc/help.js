0d4cd1a0c1594c6c48bac90fbac110c1652613ad
Olybear9 <olybear9@gmail.com>
2019-08-04 05:40:19 -0600

    Update help.js

--- Bot/Commands/misc/help.js
+++ Bot/Commands/misc/help.js
@@ -25,9 +25,7 @@ class Help {
         if (!args[0]) {
             let parents = bot.commandHandler.commands.find((c, cmd) => {
                 if (c !== cmd.name) return false;
-                if (c.list) {
-                    if (c.list === false) return false;
-                }
+                if (!cmd.list) return false; 
                 else return true;
             }).map(c => c[1]);
 
