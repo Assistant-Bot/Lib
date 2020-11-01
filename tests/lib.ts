7a414f07f7c01649e1caa3b66e0a5fe398fe576e
Cqdet <60723938+Cqdet@users.noreply.github.com>
2020-11-01 11:22:50 -0500

    Update lib test

--- tests/lib.ts
+++ tests/lib.ts
@@ -21,17 +21,26 @@ class Everyone extends Assistant.Permission {
     }
 }
 
+class Owner extends Assistant.Permission {
+    public constructor() {
+        super('Owner', 100);
+    }
+
+    public can(msg: Assistant.Message<Eris.Message>): boolean {
+        return msg.member?.guild.ownerID === msg.member?.id
+    }
+}
+
 class PingCommand extends Assistant.Command {
     public constructor() {
         super('ping', 'ping', 'Get the api response time from the bot to discord.', {
             argOptions: {
                 api: 3,
-                wrap: true
+                wrap: false
             },
             cooldown: 0,
             disabledEvents: [],
         });
-        this.label = 'ping';
         this.permissions = [
             new Everyone,
             new AdminPermission
@@ -39,15 +48,43 @@ class PingCommand extends Assistant.Command {
     }
 
     public async onRun(client: Eris.Client, msg: Assistant.Message<Eris.Message>, args: string[]): Promise<void> {
-        msg.channel.createMessage('Pong!');
+        const m = await msg.channel.createMessage('Pinging...');
+        const diff = Math.floor(m.timestamp - (msg?.timestamp || Date.now()));
+        m.edit(`Pong! \`${diff}ms\n\`API Pong! \`${msg.channel.guild.shard.latency}ms\``)
+
+    };
+}
+
+class TestCommand extends Assistant.Command {
+    public constructor() {
+        super('test', 'test', 'Test command lol.', {
+            argOptions: {
+                api: 3,
+                wrap: false,
+                permissions: [
+                    [0, new Everyone], // Will run if you do '!test arg'
+                    [1, new Owner] // Will only work if you are owner and do '!test arg ownerArg'
+                ],
+            },
+            cooldown: 0,
+            disabledEvents: [],
+        });
+        this.permissions = [
+            new Everyone
+        ];
+    }
+
+    public async onRun(client: Eris.Client, msg: Assistant.Message<Eris.Message>, args: string[]): Promise<void> {
+        console.log('Owner only command!')
+
     };
 }
 
 Assistant.PermissionManager.registerAll(
-    new Everyone, new AdminPermission
+    new Everyone, new AdminPermission, new Owner
 );
 
-const client = new Eris.Client("NzMzOTIwNzkwNDA3MjgyNzU4.XxKLAA.6ro13UZQtvaZVXJggAJZPJJ7Neg");
+const client = new Eris.Client("NzHi.Your.Bad-Lol");
 
 client.on('ready', () => {
     console.log('Ready!');
@@ -58,6 +95,6 @@ const handler = new Assistant.CommandHandler(client, {
     allowBots: true,
     allowMention: true
 });
-handler.registerModule(new Assistant.Module('Generic', [ new PingCommand ]));
+handler.registerModule(new Assistant.Module('Generic', [ new PingCommand, new TestCommand ]));
 handler.start();
 client.connect();
