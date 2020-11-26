71713d664602fbd8b27752b1ba482d963240f209
Olybear9 <olybear9@gmail.com>
2020-11-25 23:42:21 -0600

    chore: Undo, ignore previous commit to this file

--- src/data/DataStore.ts
+++ src/data/DataStore.ts
@@ -12,11 +12,6 @@
  * unless given explicit permission to do so. If you have not been given explicit
  * permission to view or modify this software you should take the appropriate actions
  * to remove this software from your device immediately.
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
  */
 
 import { Payload } from "../net/ws/packet/Packet.ts";
