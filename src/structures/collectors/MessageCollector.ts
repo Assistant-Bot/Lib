2ad16eec42c323877dfaf8f7e44884932861f99a
Cqdet <cadetmercury615@gmail.com>
2021-01-30 19:03:12 -0500

    📝chore(collector): Fix some stuff

--- src/structures/collectors/MessageCollector.ts
+++ src/structures/collectors/MessageCollector.ts
@@ -1,5 +1,6 @@
 import Message from "../Message.ts";
 import Client from "../../Client.ts";
+import { MuxAsyncIterator } from "https://deno.land/std/async/mod.ts";
 
 export type MessageFilterType = (msg: Message) => boolean;
 
@@ -19,16 +20,24 @@ export type MessageFilterType = (msg: Message) => boolean;
  * })
  */
 
-// export default class MessageCollector implements AsyncIterableIterator<Message> {
-// 	private client: Client;
-// 	private filter?: MessageFilterType
+export default class MessageCollector implements AsyncIterable<Message> {
+	private client: Client;
+	private limit: number
+	private filter?: MessageFilterType
 
-// 	public constructor(client: Client, filter?: MessageFilterType) {
-// 		this.client = client;
-// 		this.filter = filter;
-// 	}
+	public constructor(client: Client, limit: number, filter?: MessageFilterType) {
+		this.client = client;
+		this.limit = limit;
+		this.filter = filter;
+	}
 
-// 	[Symbol.asyncIterator](): AsyncIterableIterator<Message> {
-// 		// TO DO
-// 	}
-// }
+	private async *iterateMessages(iter: MuxAsyncIterator<Message>): AsyncIterableIterator<Message> {
+
+	}
+
+	[Symbol.asyncIterator](): AsyncIterableIterator<Message> {
+		const mux: MuxAsyncIterator<Message> = new MuxAsyncIterator();
+		mux.add(this.iterateMessages(mux));
+		return mux.iterate();
+	}
+}
