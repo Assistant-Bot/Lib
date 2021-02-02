139c7a6398b784af1e081d32723a72b8c566bcb4
Olybear9 <olybear9@gmail.com>
2021-02-02 15:21:06 -0600

    🔨 Add a hack test that closes deno with an exit code of 0

---
+++ tests/zExit.test.ts
@@ -0,0 +1,29 @@
+/***
+ *                    _     _              _
+ *      /\           (_)   | |            | |
+ *     /  \   ___ ___ _ ___| |_ __ _ _ __ | |_
+ *    / /\ \ / __/ __| / __| __/ _` | '_ \| __|
+ *   / ____ \\__ \__ \ \__ \ || (_| | | | | |_
+ *  /_/    \_\___/___/_|___/\__\__,_|_| |_|\__|
+ *
+ * Copyright (C) 2020 Bavfalcon9
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ */
+import {
+	Client
+} from "../mod.ts";
+import { assertEquals } from "https://deno.land/std@0.85.0/testing/asserts.ts";
+
+/**
+ * Connects to the gateway, disconnects, then resumes.
+ */
+Deno.test({
+	name: 'Exit and Assume passed',
+	fn: async () => {
+		Deno.exit()
+	}
+});
